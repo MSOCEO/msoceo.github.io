0b786092defe07ea0f859dd835d1291d619a360d
M <m@MacBook-Air.local>
2026-05-23 17:07:18 +0800

    chore: update site URL for Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://msoceo.github.io',
+	site: 'https://msoceo.pages.dev',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
