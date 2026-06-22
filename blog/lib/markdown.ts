53fefe4022432ef6437ddbdfb6b7a97ded274150
MSOCEO <msoceo@vip.qq.com>
2026-06-23 01:28:51 +0800

    cleanup: remove blog/ directory and restore package.json build script

--- blog/lib/markdown.ts
+++
@@ -1,8 +0,0 @@
-import { remark } from "remark";
-import html from "remark-html";
-import gfm from "remark-gfm";
-
-export async function markdownToHtml(markdown: string): Promise<string> {
-  const result = await remark().use(gfm).use(html).process(markdown);
-  return result.toString();
-}
